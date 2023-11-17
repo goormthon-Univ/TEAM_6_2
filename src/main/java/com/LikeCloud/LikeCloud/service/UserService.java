f121c16624b3aea548fe31bb1f6975d7b8204e13
JungYoonShin <sju08227@naver.com>
2023-11-18 00:17:06 +0900

    Feat: 닉네임 중복여부 조회 기능 구현

--- src/main/java/com/LikeCloud/LikeCloud/service/UserService.java
+++ src/main/java/com/LikeCloud/LikeCloud/service/UserService.java
@@ -3,10 +3,11 @@ package com.LikeCloud.LikeCloud.service;
 import com.LikeCloud.LikeCloud.dto.SignUpRequestDto;
 import com.LikeCloud.LikeCloud.dto.UserLoginRequestDto;
 import com.LikeCloud.LikeCloud.dto.UserLoginResponseDto;
+import com.LikeCloud.LikeCloud.dto.UserNickNameResponseDto;
 //import com.LikeCloud.LikeCloud.dto.UserNickNameResponseDto;
 
 public interface UserService {
     void saveUser(SignUpRequestDto signUpRequestDto);
     UserLoginResponseDto findUser(UserLoginRequestDto userLoginRequestDto);
-//    UserNickNameResponseDto findByUserNickname(String Nickname);
+    UserNickNameResponseDto findByUserNickname(String Nickname);
 }
